a4fc014a7bd282cfb36833344e069e23af1566c9
Czapkin <62288315+Czapkin@users.noreply.github.com>
2021-12-13 20:42:13 +0100

    Added and programmed new tracks

--- Assets/Play.cs
+++ Assets/Play.cs
@@ -46,19 +46,39 @@ public class Play : MonoBehaviour
     private AudioClip clip;
     private AudioClip generatedClip;
     
-    public AudioSource test;
+    public AudioSource generated;
     public AudioSource drums;
     public AudioSource background;
-    public AudioSource secondbackground;
+    public AudioSource custom;
 
-    private Button setBackground;
-
-    private Slider backgroundVolumeSlider;
+    public Button setBackground;
+    public Slider backgroundVolumeSlider;
+    public Toggle muteBackground;
+    public Toggle loopBackground;
+    public InputField backgroundStartOffset;
+    
+    public Button setDrums;
+    public Slider drumsVolumeSlider;
+    public Toggle muteDrums;
+    public Toggle loopDrums;
+    public InputField drumsStartOffset;
+    
+    public Button setCustom;
+    public Slider customVolumeSlider;
+    public Toggle muteCustom;
+    public Toggle loopCustom;
+    public InputField customStartOffset;
+    
+    public Button setGenerated;
+    public Slider generatedVolumeSlider;
+    public Toggle muteGenerated;
+    public Toggle loopGenerated;
+    public InputField generatedStartOffset;
 
     void Start()
     {
-        setBackground = GameObject.Find("SetBackground").GetComponent<Button>();
-        backgroundVolumeSlider = GameObject.Find("BackgroundVolume").GetComponent<Slider>();
+        //setBackground = GameObject.Find("SetSound").GetComponent<Button>();
+        //backgroundVolumeSlider = GameObject.Find("BackgroundVolume").GetComponent<Slider>();
         
         changeBPM();
         setChanceToSkip();
@@ -81,11 +101,161 @@ public class Play : MonoBehaviour
 
     public void changeVolume(int track)
     {
-        
         switch (track)
         {
             case 0:
                 background.volume = backgroundVolumeSlider.value;
+                break;
+            case 1:
+                drums.volume = drumsVolumeSlider.value;
+                break;
+            case 2:
+                custom.volume = customVolumeSlider.value;
+                break;
+            case 3:
+                generated.volume = generatedVolumeSlider.value;
+                break;
+            
+        }
+    }
+
+    public void muteTrack(int track)
+    {
+        switch (track)
+        {
+            case 0:
+                background.mute = muteBackground.isOn;
+                break;
+            case 1:
+                drums.mute = muteDrums.isOn;
+                break;
+            case 2:
+                custom.mute = muteCustom.isOn;
+                break;
+            case 3:
+                generated.mute = muteGenerated.isOn;
+                break;
+        }
+    }
+
+    public void loopTrack(int track)
+    {
+        switch (track)
+        {
+            case 0:
+                background.loop = loopBackground.isOn;
+                break;
+            case 1:
+                drums.loop = loopDrums.isOn;
+                break;
+            case 2:
+                custom.loop = loopCustom.isOn;
+                break;
+            case 3:
+                generated.loop = loopGenerated.isOn;
+                break;
+        }
+    }
+
+    public void changeStartOffset(int track)
+    {
+        switch (track)
+        {
+            case 0:
+                try
+                {
+                    backgroundStartOffset.text = float.Parse(backgroundStartOffset.text.Replace(".", ",")).ToString("0.00");
+                    background.time = float.Parse(backgroundStartOffset.text);
+                }
+                catch (FormatException e)
+                {
+                    backgroundStartOffset.text = "0,00";
+                    background.time = float.Parse(backgroundStartOffset.text);
+                }
+                break;
+            case 1:
+                try
+                {
+                    drumsStartOffset.text = float.Parse(drumsStartOffset.text.Replace(".", ",")).ToString("0.00");
+                    drums.time = float.Parse(drumsStartOffset.text);
+                }
+                catch (FormatException e)
+                {
+                    drumsStartOffset.text = "0,00";
+                    drums.time = float.Parse(drumsStartOffset.text);
+                }
+                break;
+            case 2:
+                try
+                {
+                    customStartOffset.text = float.Parse(customStartOffset.text.Replace(".", ",")).ToString("0.00");
+                    custom.time = float.Parse(customStartOffset.text);
+                }
+                catch (FormatException e)
+                {
+                    customStartOffset.text = "0,00";
+                    custom.time = float.Parse(customStartOffset.text);
+                }
+                break;
+            case 3:
+                try
+                {
+                    generatedStartOffset.text = float.Parse(generatedStartOffset.text.Replace(".", ",")).ToString("0.00");
+                    generated.time = float.Parse(generatedStartOffset.text);
+                }
+                catch (FormatException e)
+                {
+                    generatedStartOffset.text = "0,00";
+                    generated.time = float.Parse(generatedStartOffset.text);
+                }
+                break;
+        }
+    }
+
+    public void resetSettings(int track)
+    {
+        switch (track)
+        {
+            case 0:
+                backgroundStartOffset.text = "0,00";
+                background.time = float.Parse(backgroundStartOffset.text);
+                background.loop = true;
+                loopBackground.isOn = true;
+                background.mute = false;
+                muteBackground.isOn = false;
+                background.volume = 0.5f;
+                backgroundVolumeSlider.value = 0.5f;
+                break;
+
+            case 1:
+                drumsStartOffset.text = "0,00";
+                drums.time = float.Parse(backgroundStartOffset.text);
+                drums.loop = true;
+                loopDrums.isOn = true;
+                drums.mute = false;
+                muteDrums.isOn = false;
+                drums.volume = 0.5f;
+                drumsVolumeSlider.value = 0.5f;
+                break;
+            case 2:
+                customStartOffset.text = "0,00";
+                custom.time = float.Parse(backgroundStartOffset.text);
+                custom.loop = true;
+                loopCustom.isOn = true;
+                custom.mute = false;
+                muteCustom.isOn = false;
+                custom.volume = 0.5f;
+                customVolumeSlider.value = 0.5f;
+                break;
+            case 3:
+                generatedStartOffset.text = "0,00";
+                generated.time = float.Parse(backgroundStartOffset.text);
+                generated.loop = true;
+                loopGenerated.isOn = true;
+                generated.mute = false;
+                muteGenerated.isOn = false;
+                generated.volume = 0.5f;
+                generatedVolumeSlider.value = 0.5f;
                 break;
         }
     }
@@ -108,6 +278,18 @@ public class Play : MonoBehaviour
                 background.clip = generatedClip;
                 setBackground.GetComponentInChildren<Text>().text = Path.GetFileName(soundPath);
                 break;
+            case 1:
+                drums.clip = generatedClip;
+                setDrums.GetComponentInChildren<Text>().text = Path.GetFileName(soundPath);
+                break;
+            case 2:
+                custom.clip = generatedClip;
+                setCustom.GetComponentInChildren<Text>().text = Path.GetFileName(soundPath);
+                break;
+            case 3:
+                generated.clip = generatedClip;
+                setGenerated.GetComponentInChildren<Text>().text = Path.GetFileName(soundPath);
+                break;
         }
     }
 
@@ -118,7 +300,7 @@ public class Play : MonoBehaviour
         return request;
     }
 
-    public void changeClip(int track)
+    public void setClip(int track)
     {
         if (!modalOpened)
         {
@@ -126,13 +308,13 @@ public class Play : MonoBehaviour
             trackNumber = track;
             modalOpened = true;
         }
-
     }
     
     void GotFile(FileSelector.Status status, string path){
         Debug.Log("File Status : "+status+", Path : "+path);
         if(status.ToString() == "Successful")
         {
+            resetSettings(trackNumber);
             soundPath = path;
             StartCoroutine(LoadAudio());
         }
@@ -241,68 +423,68 @@ public class Play : MonoBehaviour
 
     public void normalPitch()
     {
-        test.pitch = 1;
-        test.Play();
+        generated.pitch = 1;
+        generated.Play();
     }
     
     public void pitch1()
     {
-        test.pitch = (float)Math.Pow(2, 1f / 12f);//(float)Math.Pow(1.0594630943592953, 1);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 1f / 12f);//(float)Math.Pow(1.0594630943592953, 1);
+        generated.Play();
     }
 
     public void pitch2()
     {
-        test.pitch = (float)Math.Pow(2, 2f / 12f);//(float)Math.Pow(1.0594630943592953, 2);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 2f / 12f);//(float)Math.Pow(1.0594630943592953, 2);
+        generated.Play();
     }
     
     public void pitch3()
     {
-        test.pitch = (float)Math.Pow(2, 3f / 12f);//(float)Math.Pow(1.0594630943592953, 3);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 3f / 12f);//(float)Math.Pow(1.0594630943592953, 3);
+        generated.Play();
     }
     
     public void pitch4()
     {
-        test.pitch = (float)Math.Pow(2, 4f / 12f);//(float)Math.Pow(1.0594630943592953, 4);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 4f / 12f);//(float)Math.Pow(1.0594630943592953, 4);
+        generated.Play();
     }
     
     public void pitch5()
     {
-        test.pitch = (float)Math.Pow(2, 5f / 12f);//(float)Math.Pow(1.0594630943592953, 5);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 5f / 12f);//(float)Math.Pow(1.0594630943592953, 5);
+        generated.Play();
     }
     public void pitch6()
     {
-        test.pitch = (float)Math.Pow(2, 6f / 12f);//(float)Math.Pow(1.0594630943592953, 6);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 6f / 12f);//(float)Math.Pow(1.0594630943592953, 6);
+        generated.Play();
     }
     public void pitch7()
     {
-        test.pitch = (float)Math.Pow(2, 7f / 12f);//(float)Math.Pow(1.0594630943592953, 7);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 7f / 12f);//(float)Math.Pow(1.0594630943592953, 7);
+        generated.Play();
     }
     public void pitch8()
     {
-        test.pitch = (float)Math.Pow(2, 8f / 12f);//(float)Math.Pow(1.0594630943592953, 8);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 8f / 12f);//(float)Math.Pow(1.0594630943592953, 8);
+        generated.Play();
     }
     public void pitch9()
     {
-        test.pitch = (float)Math.Pow(2, 9f / 12f);//(float)Math.Pow(1.0594630943592953, 9);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 9f / 12f);//(float)Math.Pow(1.0594630943592953, 9);
+        generated.Play();
     }
     public void pitch10()
     {
-        test.pitch = (float)Math.Pow(2, 10f / 12f);//(float)Math.Pow(1.0594630943592953, 10);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 10f / 12f);//(float)Math.Pow(1.0594630943592953, 10);
+        generated.Play();
     }
     public void pitch11()
     {
-        test.pitch = (float)Math.Pow(2, 11f / 12f);//(float)Math.Pow(1.0594630943592953, 11);
-        test.Play();
+        generated.pitch = (float)Math.Pow(2, 11f / 12f);//(float)Math.Pow(1.0594630943592953, 11);
+        generated.Play();
     }
     
 
@@ -327,7 +509,7 @@ public class Play : MonoBehaviour
                    rand = Random.Range(0, 8);
                }
                 
-               test.pitch = usedSounds[rand];
+               generated.pitch = usedSounds[rand];
 
                if (previousRand == rand)
                    replayCounter++;
@@ -338,7 +520,7 @@ public class Play : MonoBehaviour
 
                if (gap < chanceToSkip)
                {
-                   test.Play();
+                   generated.Play();
                    pitchText.text = rand.ToString();
                    
                }
